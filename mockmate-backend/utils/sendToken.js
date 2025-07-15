01e0c4eb7274a93a08ef5079c6395e0492e47651
Subrata Lodh <subrata_ug_23@ece.nits.ac.in>
2025-07-15 18:55:58 +0530

    Update sendToken.js

--- mockmate-backend/utils/sendToken.js
+++ mockmate-backend/utils/sendToken.js
@@ -7,7 +7,8 @@ export const sendToken = (user, message, statusCode, res) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      sameSite: "Lax",
+      sameSite: "None",
+      secure: true
     })
     .json({
       success: true,
